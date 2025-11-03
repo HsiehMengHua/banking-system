ead176d9bd6afcb8dcb9de646efa8f1e51cecf25
MH <qprs71@gmail.com>
2025-11-03 20:25:17 +0800

    enable run via HTTPS on local

--- main.go
+++ main.go
@@ -8,13 +8,15 @@ import (
 	"github.com/joho/godotenv"
 )
 
+var env string
+
 func init() {
 	loadEnv()
 	database.Connect()
 }
 
 func loadEnv() {
-	env := os.Getenv("APP_ENV")
+	env = os.Getenv("APP_ENV")
 	if env == "" {
 		env = "development"
 	}
@@ -29,5 +31,10 @@ func loadEnv() {
 
 func main() {
 	r := router.Setup()
-	r.Run() // listens on 0.0.0.0:8080 by default
+
+	if env == "development" && os.Getenv("RUN_HTTPS") == "y" {
+		r.RunTLS(":8444", os.Getenv("CERT_FILE"), os.Getenv("CERT_KEY"))
+	} else {
+		r.Run(os.Getenv("APP_PORT"))
+	}
 }
